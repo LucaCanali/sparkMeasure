60bcc89336194f0e6e15f24ea09d6e94edad7086
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2025-07-02 10:54:06 +0200

    Add GitHub Actions matrix build for Spark 3 and 4
    Implemented a matrix build strategy to run integration test against both Spark 3.x and Spark 4.x.
    Ensures CI coverage for multiple Spark versions and compatibility validation.

--- e2e/build.sh
+++ e2e/build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
-
 # Build image containing integration tests for sparkMeasure over Kubernetes
-
 # @author  Fabrice Jammes
 
 set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 PROJECT_DIR=$(cd "$DIR/.."; pwd -P)
+spark_image_tag="3.5.6-scala2.12-java17-python3-ubuntu"
 
 usage() {
   cat << EOD
@@ -16,16 +14,18 @@ usage() {
 Usage: `basename $0` [options]
 
   Available options:
-    -h          this message
+    -h        this message
+    -i        spark image tag to use as base image (default: 3.5.6-scala2.12-java17-python3-ubuntu)
 
 Build image containing integration tests for sparkMeasure over Kubernetes
 EOD
 }
 
 # get the options
-while getopts h c ; do
+while getopts hi: c ; do
     case $c in
 	    h) usage ; exit 0 ;;
+	    i) spark_image_tag="$OPTARG" ;;
 	    \?) usage ; exit 2 ;;
     esac
 done
@@ -34,8 +34,13 @@ shift `expr $OPTIND - 1`
 ciux ignite --selector build $PROJECT_DIR
 . $PROJECT_DIR/.ciux.d/ciux_build.sh
 
+scala_version=$(echo "$spark_image_tag" | grep -oP 'scala\K[0-9]+\.[0-9]+')
+
 echo "Building Docker image for sparkMeasure integration tests"
-docker image build --tag "$CIUX_IMAGE_URL" "$PROJECT_DIR"
+docker image build  \
+  --build-arg "spark_image_tag=$spark_image_tag" \
+  --build-arg "scala_version=$scala_version" \
+  --tag "$CIUX_IMAGE_URL" "$PROJECT_DIR"
 
 echo "Build successful"
 
